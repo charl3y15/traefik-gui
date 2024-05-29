75a35602e90b5a48fa352e3ab09a5ce0ed5aadc2
L. Wlcek <luca@it-rahn.de>
2024-05-29 14:09:19 +0200

    working prototype

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,6 @@
 import Database from 'better-sqlite3';
+import YAML from 'yaml';
+import fs from 'fs';
 
 let db: Database.Database | null = null;
 
@@ -18,3 +20,33 @@ export function getDB() {
 
   return db;
 }
+
+function generateTraefikConfig() {
+  const db = getDB();
+  const routes = db.prepare('SELECT * FROM http_routes').all();
+
+  const config = {
+    http: {
+      routers: {},
+      services: {}
+    }
+  };
+
+  routes.forEach(route => {
+    config.http.routers[route.name] = {
+      rule: route.rule,
+      service: `${route.name}-service`,
+      entryPoints: ['web']
+    };
+
+    config.http.services[`${route.name}-service`] = {
+      loadBalancer: {
+        servers: [{ url: `http://${route.target}` }]
+      }
+    };
+  });
+
+  fs.writeFileSync('dynamic_config.yml', YAML.stringify(config, { indent: 2 }));
+}
+
+generateTraefikConfig();
