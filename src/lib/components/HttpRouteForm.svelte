6ae72ba08d08adc779b14f7b4dad5ff2c62186e5
Luca Wlcek <luca@it-rahn.de>
2024-05-29 16:30:18 +0200

    reorganized route editor

---
+++ src/lib/components/HttpRouteForm.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { HttpRoute } from "$lib/types";
+
+  export let route: HttpRoute;
+</script>
+
+<div>
+  <div class="form-control mb-2">
+    <label for="name" class="label">
+      <span class="label-text">Name:</span>
+    </label>
+    <input
+      id="name"
+      type="text"
+      bind:value={route.name}
+      class="input input-bordered"
+      name="name"
+      required
+    />
+  </div>
+  <div class="form-control mb-2">
+    <label for="target" class="label">
+      <span class="label-text">Target:</span>
+    </label>
+    <input
+      id="target"
+      type="text"
+      bind:value={route.target}
+      class="input input-bordered"
+      name="target"
+      required
+    />
+  </div>
+  <div class="form-control mb-2">
+    <label for="rule" class="label">
+      <span class="label-text">Rule:</span>
+    </label>
+    <input
+      id="rule"
+      type="text"
+      bind:value={route.rule}
+      class="input input-bordered"
+      name="rule"
+      required
+    />
+  </div>
+</div>
--- src/routes/http_routes/+page.svelte
+++ src/routes/http_routes/+page.svelte
@@ -1,50 +1,62 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
-  import type { HttpRoute } from '$lib/types';
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+  import type { HttpRoute } from "$lib/types";
+  import HttpRouteForm from "$lib/components/HttpRouteForm.svelte";
 
   let http_routes = writable<HttpRoute[]>([]);
-  let name = '';
-  let target = '';
-  let rule = '';
-  let errorMessage = '';
+  let route: HttpRoute = {
+    id: 0,
+    name: "",
+    target: "",
+    rule: "",
+  };
+
+  let errorMessage = "";
 
   async function fetchRoutes() {
-    const response = await fetch('/api/http_routes');
+    const response = await fetch("/api/http_routes");
     const data = await response.json();
     http_routes.set(data.httpRoutes as HttpRoute[]);
   }
 
   async function addRoute() {
-    const response = await fetch('/api/http_routes', {
-      method: 'POST',
+    // clone route and remove id
+    let post_route: Partial<HttpRoute> = { ...route };
+    delete post_route.id;
+    const response = await fetch("/api/http_routes", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, target, rule })
+
+      body: JSON.stringify(post_route),
     });
 
     if (response.ok) {
       await fetchRoutes(); // Refresh the list after adding a new route
-      name = '';
-      target = '';
-      rule = '';
-      errorMessage = '';
+      route = {
+        id: 0,
+        name: "",
+        target: "",
+        rule: "",
+      };
+      errorMessage = "";
     } else {
       const errorData = await response.json();
-      errorMessage = errorData.error || 'Failed to add route';
+      errorMessage = errorData.error || "Failed to add route";
     }
   }
 
   async function deleteRoute(id: number) {
     const response = await fetch(`/api/http_routes/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     });
 
     if (response.ok) {
       await fetchRoutes(); // Refresh the list after deleting a route
     } else {
-      console.error('Failed to delete route');
+      console.error("Failed to delete route");
     }
   }
 
@@ -59,24 +71,7 @@
   {/if}
 
   <form on:submit|preventDefault={addRoute} class="mb-4">
-    <div class="form-control mb-2">
-      <label for="name" class="label">
-        <span class="label-text">Name:</span>
-      </label>
-      <input id="name" type="text" bind:value={name} class="input input-bordered" required>
-    </div>
-    <div class="form-control mb-2">
-      <label for="target" class="label">
-        <span class="label-text">Target:</span>
-      </label>
-      <input id="target" type="text" bind:value={target} class="input input-bordered" required>
-    </div>
-    <div class="form-control mb-2">
-      <label for="rule" class="label">
-        <span class="label-text">Rule:</span>
-      </label>
-      <input id="rule" type="text" bind:value={rule} class="input input-bordered" required>
-    </div>
+    <HttpRouteForm bind:route />
     <button type="submit" class="btn btn-primary">Add Route</button>
   </form>
 
@@ -96,7 +91,9 @@
           <td>{route.target}</td>
           <td>{route.rule}</td>
           <td>
-            <button on:click={() => deleteRoute(route.id)} class="btn btn-error">Delete</button>
+            <button on:click={() => deleteRoute(route.id)} class="btn btn-error"
+              >Delete</button
+            >
           </td>
         </tr>
       {/each}
